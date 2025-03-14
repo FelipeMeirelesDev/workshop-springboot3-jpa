548d08544eda391f1a17a3426d57b93487356670
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2025-03-14 13:04:26 -0300

    Adicionando o banco de dados H2 e JPA

--- src/main/java/com/educandoweb/curso/entities/Usuario.java
+++ src/main/java/com/educandoweb/curso/entities/Usuario.java
@@ -1,12 +1,18 @@
 package com.educandoweb.curso.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table (name = "tb_usuario")
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
