089cb4237c6104e0642c67cf9cc42d114b056198
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2025-03-19 19:45:33 -0300

    Camada de serviço e componente de registro

--- src/main/java/com/educandoweb/curso/resources/UsuarioResources.java
+++ src/main/java/com/educandoweb/curso/resources/UsuarioResources.java
@@ -1,19 +1,32 @@
 package com.educandoweb.curso.resources;
 
 import com.educandoweb.curso.entities.Usuario;
+import com.educandoweb.curso.services.UsuarioServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResources {
 
+    @Autowired
+    private UsuarioServices service;
+
     @GetMapping
-    public ResponseEntity<Usuario> findAll(){
-      Usuario u = new Usuario(1L,"Felipe","felipe@gmail.com","99999","12345");
-      return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> list = service.findAll();
+      return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Usuario> findById(@PathVariable Long id){
+        Usuario obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 }
